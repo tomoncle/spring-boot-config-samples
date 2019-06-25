39d31c2c7a8aedb17091a9600316c4d52d4fb7e1
tomoncle <1123431949@qq.com>
2019-06-25 14:38:24 +0800

    update jar to war.

--- spring-boot-config-samples-tomcat-druid/src/main/java/com/tomoncle/app/Application.java
+++ spring-boot-config-samples-tomcat-druid/src/main/java/com/tomoncle/app/Application.java
@@ -4,11 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.tomoncle.app"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
 
     public static void main(String[] args) {
         ApplicationContext application = SpringApplication.run(Application.class, args);
