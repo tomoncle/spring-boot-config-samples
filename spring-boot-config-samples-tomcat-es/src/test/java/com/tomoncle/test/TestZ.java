032c7eea8af3534d7e31c6e4d48fc1a93052ee95
tomoncle <1123431949@qq.com>
2020-05-24 13:12:40 +0800

    remove config

--- spring-boot-config-samples-tomcat-es/src/test/java/com/tomoncle/test/TestZ.java
+++ spring-boot-config-samples-tomcat-es/src/test/java/com/tomoncle/test/TestZ.java
@@ -16,13 +16,82 @@
 
 package com.tomoncle.test;
 
-import java.time.Duration;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tomoncle
  */
-public class TestZ {
+public class TestZ<SOURCE, TARGET> {
     public static void main(String[] args) {
-        System.out.println(Duration.ofMinutes(1).toMillis());
+        TestZ<M, N> testZ = new TestZ<>();
+        M m = new M();
+        m.setId("abc");
+        System.out.println(testZ.copyTarget(m, N.class));
+    }
+
+    public List<TARGET> copyTarget(List<SOURCE> sources, Class<? extends TARGET> target) {
+        List<TARGET> targets = new ArrayList<>();
+        TARGET TARGET;
+        for (SOURCE SOURCE : sources) {
+            try {
+                TARGET = target.newInstance();
+                BeanUtils.copyProperties(SOURCE, TARGET);
+                targets.add(TARGET);
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return targets;
+    }
+
+    public List<SOURCE> copySource(List<TARGET> sources, Class<? extends SOURCE> target) {
+        List<SOURCE> targets = new ArrayList<>();
+        SOURCE t;
+        for (TARGET s : sources) {
+            try {
+                t = target.newInstance();
+                BeanUtils.copyProperties(s, t);
+                targets.add(t);
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return targets;
+    }
+
+    public TARGET copyTarget(SOURCE source, Class<? extends TARGET> target) {
+        try {
+            TARGET TARGET = target.newInstance();
+            BeanUtils.copyProperties(source, TARGET);
+            return TARGET;
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public SOURCE copySource(TARGET source, Class<? extends SOURCE> target) {
+        try {
+            SOURCE t = target.newInstance();
+            BeanUtils.copyProperties(source, t);
+            return t;
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Data
+    static class M {
+        private String id;
+    }
+
+    @Data
+    static class N {
+        private String id;
     }
 }
