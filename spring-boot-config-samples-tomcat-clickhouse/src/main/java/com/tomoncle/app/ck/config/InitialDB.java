5906259c5f78317200c0de37e40b1b5e44addf43
tomoncle <1123431949@qq.com>
2020-12-14 10:00:16 +0800

    add restful api demo.

--- spring-boot-config-samples-tomcat-clickhouse/src/main/java/com/tomoncle/app/ck/config/InitialDB.java
+++ spring-boot-config-samples-tomcat-clickhouse/src/main/java/com/tomoncle/app/ck/config/InitialDB.java
@@ -16,20 +16,9 @@
 
 package com.tomoncle.app.ck.config;
 
-import com.tomoncle.config.springboot.druid.DruidConfigurationProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.tomoncle.config.springboot.ch.query.StringClickHouseQuery;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author tomoncle
@@ -37,57 +26,10 @@ import java.util.Map;
 @Order(-2147483647)
 @Component
 public class InitialDB {
-    private static Logger logger = LoggerFactory.getLogger(InitialDB.class);
-    private final RestTemplate restTemplate;
-    private final DruidConfigurationProperties properties;
-
-    public InitialDB(RestTemplate restTemplate, DruidConfigurationProperties properties) {
-        this.restTemplate = restTemplate;
-        this.properties = properties;
-        initDatabase();
-    }
-
-    @SuppressWarnings("all")
-    private void initDatabase() {
-        String[] split = properties.getUrl().split("/");
-        String hostname = split[2];
-        String database = split[3].split("\\?")[0];
-        String url = "http://" + hostname + "/?" + getParameters(properties.getUsername(), properties.getPassword(), database);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("query", "CREATE TABLE IF NOT EXISTS jpa_model ( " +
+    public InitialDB(StringClickHouseQuery stringClickHouseQuery) {
+        stringClickHouseQuery.execute("CREATE TABLE IF NOT EXISTS jpa_model ( " +
                 "`event_date` Date DEFAULT CAST(now(), 'Date'), " +
                 "`uuid` String DEFAULT generateUUIDv4()" +
-                ") ENGINE = MergeTree(event_date, (uuid), 8192)");
-        HttpEntity requestBody = new HttpEntity(map, headers);
-        String object = this.restTemplate.postForObject(url, requestBody, String.class);
-        logger.info(null == object ? "create jpa_model success!" : object);
+                ") ENGINE = MergeTree(event_date, (uuid), 8192)", String.class);
     }
-
-
-    private String getParameters(String user, String password, String database) {
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("add_http_cors_header", "1");
-        parameters.put("log_queries", "1");
-        parameters.put("output_format_json_quote_64bit_integers", "1");
-        parameters.put("output_format_json_quote_denormals", "1");
-        parameters.put("user", user);
-        parameters.put("password", password);
-        parameters.put("database", database);
-        parameters.put("result_overflow_mode", "throw");
-        parameters.put("max_result_rows", "5000");
-        parameters.put("timeout_overflow_mode", "throw");
-        parameters.put("max_execution_time", "5");
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String key : parameters.keySet()) {
-            if (stringBuilder.length() > 0) {
-                stringBuilder.append("&");
-            }
-            stringBuilder.append(key).append("=").append(parameters.get(key));
-        }
-        return stringBuilder.toString();
-
-    }
-
 }
