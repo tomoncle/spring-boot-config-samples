2fc8d7ebeb3b0f08c7f54691ea04cefb826760da
tomoncle <1123431949@qq.com>
2021-04-30 10:47:34 +0800

    add restful api demo.

---
+++ spring-boot-config-samples-tomcat-restfulapi/src/main/java/com/tomoncle/app/restful/api/RestApplication.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 tomoncle
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tomoncle.app.restful.api;
+
+import lombok.SneakyThrows;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author tomoncle
+ */
+@SpringBootApplication(scanBasePackages = {"com.tomoncle.app.restful"})
+public class RestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+    @SneakyThrows
+    @Bean
+    @ConditionalOnMissingBean(name = "restTemplate")
+    public RestTemplate restTemplate() {
+        TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(
+                SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build(),
+                new NoopHostnameVerifier());
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+
+}
